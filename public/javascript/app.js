7817457292d424f9a912d113211c893309e610dd
shiva-git <ssubram1@binghamton.edu>
2017-05-09 13:14:09 -0400

    added factory functionality

---
+++ public/javascript/app.js
@@ -0,0 +1,44 @@
+(function(){
+
+	var app = angular.module('app',[]);
+
+
+	app.factory('MyFactory', function () {
+ 
+		var MyFactory = {};
+
+		MyFactory.data={
+
+			name: '',
+			email: '',
+			skills: '',
+			summary: '',
+			check: 'false'
+		};
+
+		MyFactory.checkSubmission=function() {
+			MyFactory.data.check = 'true';
+			return MyFactory.data.check;
+			// body...
+		};
+
+		return MyFactory;
+	});
+
+	app.controller('createCtrl',['MyFactory',function(MyFactory){
+		var self =this;
+		self.name=MyFactory.data.name;
+		self.email=MyFactory.data.email;
+		self.skills=MyFactory.data.skills;
+		self.summary=MyFactory.data.summary;
+		self.check=MyFactory.check;
+
+		self.checkSubmission = function(){
+
+
+			self.check = MyFactory.checkSubmission();
+		}
+	}]);
+})();
+
+/*module.exports = angulartest;*/
